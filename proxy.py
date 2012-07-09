66bb573cc0df0f0ac9279de9554abe5180981223
Bo Zhu <zhubo03@gmail.com>
2012-07-09 15:00:52 -0400

    rewrite some parts

--- proxy.py
+++ proxy.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
-# the default number of domain roots to be removed
-NUM_REMOVED_ROOTS = 3
+# the base portions of domain names to be removed
+BASE_DOMAINS = (
+        '127.0.0.1.xip.io',
+        'test.zhuzhu.org',
+        # add your domain name here
+)
 
 import tornado.web
 import tornado.ioloop
 import tornado.httpclient
+from base64 import b64decode
+from urlparse import urlparse
 
-#tornado.httpclient.AsyncHTTPClient.configure(
-#        "tornado.curl_httpclient.CurlAsyncHTTPClient"
-#)
 
-#from pprint import pprint
+CROSSDOMAIN_XML = """<?xml version="1.0" encoding="UTF-8"?>
+<cross-domain-policy>
+    <allow-access-from domain="*"/>
+</cross-domain-policy>"""
 
 
 disallowed_response_headers = frozenset([
@@ -32,22 +38,26 @@ disallowed_response_headers = frozenset([
 class ProxyHandler(tornado.web.RequestHandler):
     SUPPORTED_METHODS = ['GET', 'POST']
 
-    def _get_real_domain(self, num_removed_roots=NUM_REMOVED_ROOTS):
-        # e.g., httpbin.org.127.0.0.1.xip.io will return httpbin.org
-        d = self.request.host.split(':')[0]
-        if d.endswith('.127.0.0.1.xip.io'):
-            return d[:-17]
+    def _get_real_things(self):
+        domain = self.request.host.split(':')[0]
+        for base_domain in BASE_DOMAINS:
+            if domain.endswith(base_domain):
+                if len(domain) > len(base_domain):
+                    real_domain = domain[:-(len(base_domain) + 1)]
+                    real_url = 'http://' + real_domain + self.request.uri
+                else:
+                    real_url = self.get_argument('url')
+                    if not real_url.startswith('http://'):
+                        real_url = b64decode(real_url)
+                    real_domain = urlparse(real_url).netloc
 
-        l = d.split('.')
-        assert len(l) > num_removed_roots
-        return '.'.join(l[:(-num_removed_roots)])
+                return real_domain, real_url
+
+        # should not reach here
+        return None, None
 
     def _handle_response(self, response):
         self.set_status(response.code)
-
-        #print
-        #print self.request.uri
-        #pprint(response.headers)
 
         # check the last comment of http://goo.gl/4w5yj
         for h in response.headers.keys():
@@ -57,25 +67,33 @@ class ProxyHandler(tornado.web.RequestHandler):
                 # using only add_header will cause some problems
                 # e.g. _clear_headers_for_304 doesn't work for _list_headers
                 if len(list_values) == 1:
-                    #print h, list_values[0]
                     self.set_header(h, list_values[0])
                 else:
                     for v in list_values:
-                        #print h, v
                         self.add_header(h, v)
 
         if response.body:
             self.write(response.body)
 
-        #self.flush()
         self.finish()
 
     @tornado.web.asynchronous
     def get(self):
-        real_domain = self._get_real_domain()
-        real_url = 'http://' + real_domain + self.request.uri
-        #print real_domain
-        #print real_url
+        if self.request.uri == '/favicon.ico':
+            self.send_error(404)
+            return
+
+        elif self.request.uri == '/crossdomain.xml':
+            self.set_status(200)
+            self.set_header('Content-Type', 'text/xml')
+            self.write(CROSSDOMAIN_XML)
+            self.finish()
+            return
+
+        real_domain, real_url = self._get_real_things()
+        if not real_domain or not real_url:
+            self.send_error(403)
+            return
 
         headers = self.request.headers
         if 'Host' in headers:
@@ -112,7 +130,9 @@ application = tornado.web.Application(
 )
 
 if __name__ == "__main__":
-    print 'Open your browser and try to access'
-    print 'http://httpbin.org.127.0.0.1.xip.io:8888'
+    print 'You can try'
+    print 'http://httpbin.org.127.0.0.1.xip.io:8888/get'
+    print 'or'
+    print 'http://127.0.0.1.xip.io:8888/?url=aHR0cDovL2h0dHBiaW4ub3JnL2dldA=='
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
