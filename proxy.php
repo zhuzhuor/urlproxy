9173b16581e40da9bfb783fe9827c8550acd6d1c
Bo Zhu <zhubo03@gmail.com>
2012-07-09 15:45:04 -0400

    added choices for base64

--- proxy.php
+++ proxy.php
@@ -2,7 +2,7 @@
 
 // Proxy-on-Name PHP Version
 // Copyright (c) 2012 Bo Zhu http://zhuzhu.org
-// Licensed under the GPLv2 license.
+// Licensed under the MIT license.
 
 // Based on Simple PHP Proxy by Ben Alman
 // Project Home - http://benalman.com/projects/php-simple-proxy/
@@ -10,11 +10,11 @@
 // Dual licensed under the MIT and GPLv2 licenses.
 // http://benalman.com/about/license/
 
+
 require_once('http_response_code.php');
 
 
 $url = $_GET['url'];
-
 
 if (!$url) {
     http_response_code(400);  // bad request
@@ -22,7 +22,11 @@ if (!$url) {
     exit(400);
 }
 
+if (substr($url, 0, len('http://')) !== 'http://')
+    $url = base64_decode($url);
 
+
+// get content by using curl
 $ch = curl_init($url);
 
 if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
@@ -30,34 +34,29 @@ if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
     curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 }
 
-// always send cookies
-$cookie = array();
+$cookie = array();  // always send cookies
 foreach ($_COOKIE as $key => $value) {
     $cookie[] = $key . '=' . $value;
 }
-// always send session
-$cookie[] = SID;
+$cookie[] = SID;  // always send session
 $cookie = implode('; ', $cookie);
 
 curl_setopt($ch, CURLOPT_COOKIE, $cookie);
-
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 
-list($header, $contents) = preg_split('/([\r\n][\r\n])\\1/', curl_exec($ch), 2);
-
+list($header, $content) = preg_split('/([\r\n][\r\n])\\1/', curl_exec($ch), 2);
 $info = curl_getinfo($ch);
 
 curl_close($ch);
 
 
+// start sending response
 http_response_code((int) $info['http_code']);
 
-// Split header text into an array.
-$header_text = preg_split('/[\r\n]+/', $header);
+$header_text = preg_split('/[\r\n]+/', $header);  // split header text into an array
 
 foreach ($header_text as $header) {
     if (preg_match('/^(?:Content-Type|Content-Language|Set-Cookie):/i', $header)) {
@@ -65,7 +64,6 @@ foreach ($header_text as $header) {
     }
 }
 
-echo $contents;
-
+echo $content;
 
 ?>
