b4cd25c5d50fbec50d724de029a19a2143d36e9b
Bo Zhu <zhubo03@gmail.com>
2012-08-17 01:10:57 -0400

    added license

--- proxy.js
+++ proxy.js
@@ -1,9 +1,32 @@
+/*
+ * Copyright (C) 2012 Bo Zhu http://zhuzhu.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 
 
-// it can also support usage as a proxy server
+// Yes, it's also a proxy server
 var support_proxy = true;
 
 
@@ -114,6 +137,8 @@ http.createServer(function(request, response) {
         proxy_req.end();
     });
 }).listen(8888);
+//}).listen(8888, '127.0.0.1');
+
 
 console.log('You can try');
 console.log('http://httpbin.org.127.0.0.1.xip.io:8888/get');
